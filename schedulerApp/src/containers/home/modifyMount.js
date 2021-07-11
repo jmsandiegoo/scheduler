c532e1d5247b8d886e956b6b7bf73135c9f94c95
devKarthikRaj <rocketraj2000@gmail.com>
2021-07-11 17:01:04 +0800

    Restored previous UI code that vanished

--- schedulerApp/src/containers/home/modifyMount.js
+++ schedulerApp/src/containers/home/modifyMount.js
@@ -1,11 +1,58 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TextInput, Image, Pressable, TouchableOpacity,} from 'react-native';
 import {globalStyles} from '../../styles/global';
+import ToggleSwitch from 'rn-toggle-switch'
 
 export default function ModifyMount() {
   return (
     <View style={globalStyles.container}>
-      <Text>Modify Mount</Text>
+      <TextInput 
+        style={globalStyles.outlineInputStyle}
+        placeholder={'Camp'}
+        placeholderTextColor="#fff"
+      />
+      <View style={globalStyles.modifyMountSubtitleStyle}>
+        <Text style={globalStyles.subTitleStyle}>Starts</Text>
+        <View style={globalStyles.modifyMountSubSubtitleStyle}>
+          <Text style={globalStyles.subTitleStyle}>Date</Text>
+          <Text>         </Text>
+          <Text style={globalStyles.subTitleStyle}>Time</Text>
+        </View>
+      </View>
+      
+      <View style={globalStyles.modifyMountSubtitleStyle}>
+        <Text style={globalStyles.subTitleStyle}>Ends</Text>
+        <View style={globalStyles.modifyMountSubSubtitleStyle}>
+          <Text style={globalStyles.subTitleStyle}>Date</Text>
+          <Text>         </Text>
+          <Text style={globalStyles.subTitleStyle}>Time</Text>
+        </View>
+      </View>
+
+      <View style={globalStyles.modifyMountSubtitleStyle}>
+        <Text style={globalStyles.subTitleStyle}>Repeat</Text>
+        <ToggleSwitch
+          text={{on: '', off: '', activeTextColor: 'white', inactiveTextColor: '#B7B8BA'}}
+          textStyle={{fontWeight: 'bold'}}
+          color={{ indicator: 'white', active: 'rgba(32, 193, 173, 1)', inactive:  'rgba( 247, 247, 247, 1)', activeBorder: '#41B4A4', inactiveBorder: '#E9E9E9'}}
+          active={true}
+          disabled={false}
+          width={30}
+          radius={15}
+          onValueChange={(val) => {
+            /* your handler function... */
+          }}
+        />
+      </View>
+
+      <View style={globalStyles.modifyMountSubtitleStyle}>
+        <Text style={globalStyles.subTitleStyle}>Team</Text>
+          <Pressable>
+            <TouchableOpacity>
+              <Image source={require('../../assets/images/pencil_icon.png')} />
+            </TouchableOpacity>
+          </Pressable>
+      </View>
     </View>
   );
 }
--- schedulerApp/src/styles/global.js
+++ schedulerApp/src/styles/global.js
@@ -21,14 +21,49 @@ export const globalStyles = StyleSheet.create({
     height: 25,
     alignItems: 'center',
     justifyContent: 'center',
-    right: 30,
-    bottom: 30,
+    right: 40,
+    bottom: 40,
   },
 
   fabStyle: {
     resizeMode: 'contain',
     width:   50,
     height: 50,
+  },
+
+  outlineInputStyle: {
+    backgroundColor: '#1b1b1b',
+    borderWidth: 1,
+    borderRadius: 30,
+    paddingHorizontal: 10,
+    color: '#fff',
+    fontSize: 20,
+    paddingHorizontal: 20,
+    borderColor: '#FF3D00'
+  },
+
+  subTitleStyle: {
+    color: '#fff',
+    fontSize: 20,
+  },
+
+  modifyMountSubtitleStyle: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 20,
+  },
+
+  modifyMountSubSubtitleStyle: {
+    flexDirection: 'row', 
+    justifyContent: 'center',
+  },
+
+  buttonStyle: {
+    backgroundColor: '#EB5757',
+  },
+
+  buttonTextStyle: {
+    fontWeight: 'bold'
   }
 });
 
