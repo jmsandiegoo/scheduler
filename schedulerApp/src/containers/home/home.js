fbe093a99b1b578d1b422079032bcfb9e3ca2f9f
devKarthikRaj <rocketraj2000@gmail.com>
2021-07-11 17:01:04 +0800

    Restored previous UI code that vanished
    pulling changes from develop to viewmount_feature

--- schedulerApp/src/containers/home/home.js
+++ schedulerApp/src/containers/home/home.js
@@ -1,37 +1,11 @@
 import React from 'react';
 import {View, Text, Button, TouchableOpacity, Image} from 'react-native';
 import {globalStyles} from '../../styles/global';
-import {CalendarList} from 'react-native-calendars';
 
 export default function Home({navigation}) {
   return (
     <View style={globalStyles.container}>
       <Text>Home</Text>
-
-      <CalendarList
-        // Callback which gets executed when visible months change in scroll view. Default = undefined
-        // onVisibleMonthsChange={(months) => {
-        //   console.log('now these months are visible', months);
-        // }}
-        // Max amount of months allowed to scroll to the past. Default = 50
-        pastScrollRange={50}
-        // Max amount of months allowed to scroll to the future. Default = 50
-        futureScrollRange={50}
-        // Enable or disable scrolling of calendar list
-        scrollEnabled={true}
-        // Enable or disable vertical scroll indicator. Default = false
-        showScrollIndicator={true}
-        //Current Date
-        current={'2021-06-29'}
-        // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-        minDate={'2021-01-01'}
-        // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-        maxDate={'2099-12-31'}
-        // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
-        monthFormat={'MMM yyyy'}
-        // Do not show days of other months in month page. Default = false
-        hideExtraDays={true}
-      />
 
       <TouchableOpacity
         onPress={() => navigation.navigate('ModifyMount')}
--- schedulerApp/src/containers/settings.js
+++ schedulerApp/src/containers/settings.js
@@ -1,10 +1,47 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, TextInput} from 'react-native';
+import {globalStyles} from '../styles/global';
+import Button from 'apsl-react-native-button'
 
 export default function Settings({navigation}) {
   return (
-    <View>
-      <Text>Settings</Text>
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.subTitleStyle}>Maximum hours of continuous sentry (per combatant)</Text>
+      <View style={{paddingTop: 10, paddingBottom: 20,}}>
+        <TextInput 
+          style={globalStyles.outlineInputStyle}
+          placeholder={'Hours'}
+          placeholderTextColor="#fff"
+          keyboardType="phone-pad"
+        />
+      </View>
+
+      <Text style={globalStyles.subTitleStyle}>Maximum prowls (per combatant - weekday)</Text>
+      <View style={{paddingTop: 10, paddingBottom: 20,}}>
+        <TextInput 
+          style={globalStyles.outlineInputStyle}
+          placeholder={'Number'}
+          placeholderTextColor="#fff"
+          keyboardType="phone-pad"
+        />
+      </View>
+
+      <Text style={globalStyles.subTitleStyle}>Maximum prowls (per combatant - weekend)</Text>
+      <View style={{paddingTop: 10, paddingBottom: 20,}}>
+        <TextInput 
+          style={globalStyles.outlineInputStyle}
+          placeholder={'Number'}
+          placeholderTextColor="#fff"
+          keyboardType="phone-pad"
+        />
+      </View>
+
+      <Button 
+        style={globalStyles.buttonStyle}
+        textStyle={globalStyles.buttonTextStyle}
+      >
+        Set
+      </Button>
     </View>
   );
 }
